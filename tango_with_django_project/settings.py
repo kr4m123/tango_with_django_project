d6211f0f3b061ef60e038d4deff0cf84a63c5569
Mark Freeth <2464263F@student.gla.ac.uk>
2021-02-12 16:20:06 +0000

    lab 5 chapter 9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -21,6 +21,8 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+LOGIN_URL = 'rango:login'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -97,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
